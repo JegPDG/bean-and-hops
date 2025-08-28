57dd4ce34d5bb6625b419611274877b96e010e67
Jeg Paduga <145212230+JegPDG@users.noreply.github.com>
2025-08-28 23:37:02 +0800

    Fixing image render

--- backend/beanandhops/views.py
+++ backend/beanandhops/views.py
@@ -1,15 +1,46 @@
 from django.shortcuts import render
 from .models import MenuItem, Reviews, Reply
 from rest_framework.generics import ListAPIView
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework import viewsets
 from .serializers import MenuItemSerializer, ReviewSerializers
-
+from collections import defaultdict
 
 # Create your views here.
-class MenuItemAPIView(ListAPIView):
+class MenuItemAPIView(viewsets.ModelViewSet):
   queryset = MenuItem.objects.all()
   serializer_class = MenuItemSerializer
-  
+
+  @action(detail=False, methods=['get'], url_path='grouped-items/(?P<subtype>[^/.]+)' )
+  def group_by_subtype(self, request, subtype=None):
+    items = MenuItem.objects.filter(mnu_subtype__name__iexact=subtype)
+    serializer = self.get_serializer(items, many=True)
+    return Response({
+      "Subtype": subtype,
+      "subtypeItem": serializer.data
+    })
+    
+  @action(detail=False, methods=['get'], url_path='grouped-category/(?P<category>[^/.]+)')
+  def group_by_category(self, request, category=None):
+    items = MenuItem.objects.filter(mnu_category__name__iexact=category)
+    serializer = self.get_serializer(items, many=True)
+
+    grouped = defaultdict(list)
+    for item in serializer.data:
+        grouped[item["mnu_subtype"]].append(item)
+
+    result = [{
+        "Category": category,
+        "Subtypes": [
+            {
+                "Subtype": subtype,
+                "Items": items
+            } for subtype, items in grouped.items()
+        ]
+    }]
+    return Response(result)
+
 class ReviewsAPIViewSet(viewsets.ModelViewSet):
   queryset = Reviews.objects.all()
   serializer_class = ReviewSerializers
