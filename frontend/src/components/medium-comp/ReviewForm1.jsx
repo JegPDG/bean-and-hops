f4256728fae1a94fd82fac3d2ed436721cafa05f
Jeg Paduga <145212230+JegPDG@users.noreply.github.com>
2025-09-23 22:35:47 +0800

    review

--- frontend/src/components/medium-comp/ReviewForm1.jsx
+++ frontend/src/components/medium-comp/ReviewForm1.jsx
@@ -1,87 +1,93 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StarIcon as StarOutline, ArrowTurnDownRightIcon  } from '@heroicons/react/24/outline'
-import { StarIcon as StarSolid } from '@heroicons/react/24/solid'
+import { PlusIcon, StarIcon as StarSolid } from '@heroicons/react/24/solid'
 
 import Reply from './Reply'
+import { useAuth } from '../../context/AuthContext'
 
 
-const ReviewForm1 = ({profilePic, username, rate, itemReviewed, text, image, dateTime, replies}) => {
+const ReviewForm1 = ({itemReviewed}) => {
+  // {profilePic, username, rate, itemReviewed, text, image, dateTime, replies}
+  // user in seralizers 'id', 'first_name', 'last_name', 'email'
+
+  const { user, isAuthenticated } = useAuth();
+  const [formData, setFormData] = useState({
+    rvw_item: itemReviewed || '',
+    rvw_rate: '',
+    rvw_text: '',
+    rvw_image: null
+  });
+
+  const handleImageChange = (e) => {
+  const file = e.target.files[0];
+    if (file) {
+      setFormData({
+        ...formData,
+        rvw_image: file
+      });
+      
+      // Create preview
+      const reader = new FileReader();
+      reader.onload = (e) => setPreviewImage(e.target.result);
+      reader.readAsDataURL(file);
+    }
+  };
   
 
   return (
     <div className='w-full m-auto flex max-w-lg gap-4 pt-4 shrink-0'>
-
       {/* Profile picture */}
       <div className='size-12 aspect-square rounded-[50%] bg-amber-50 flex overflow-hidden shrink-0'>
-        <img className='size-12 object-cover shrink-0' src={profilePic} alt="" />
+        <img className='size-12 object-cover shrink-0' src={user?.first_name} alt="" />
       </div>
 
       <div className='w-full '> 
         <div className='flex shrink-0'>
 
           {/* Username */}
-          <p className='font-bold'>{username}</p> 
-          <p> &nbsp; | &nbsp;</p>
+          <p className='font-bold'></p> 
+          {/* <p> &nbsp; | &nbsp;</p> */}
 
-          {/* Rate */}
-          <div className='flex items-center'>
-            <StarSolid className='size-4'></StarSolid>
-            <StarSolid className='size-4'></StarSolid>
-            <StarSolid className='size-4'></StarSolid>
-            <StarOutline className='size-4'></StarOutline>
-            <StarOutline className='size-4'></StarOutline>
-            <p>{rate}</p>
-          </div>
         </div>
 
         <div>
 
-          {/* Item reviewed */}
-          <p className='cursor-pointer'> 
-            <span className='font-bold'>{itemReviewed}</span> 
-            {itemReviewed && (
-              <span className='text-white/50 text-xs italic'>&nbsp; View in Menu</span>
-            )}
-          </p>
+            {/* text review */}
+            <textarea 
+              name="comment" 
+              id="review-input"
+              className='w-full bg-bg-dark-400 text-sm resize-none p-[4px]'
+              ></textarea>
+            {/* <p className='text-sm'> {text} </p> */}
 
-          {/* text review */}
-          <textarea 
-            name="comment" 
-            id="review-input"
-            className='w-full bg-bg-dark-400 text-sm resize-none p-[4px]'
-            ></textarea>
-          <p className='text-sm'> {text} </p>
+            
+            <div className='w-full h-[0.5px] mt-2 bg-white/10'></div>
 
-          {/* Image for the review */}
-          { image &&
-            (
-              <div className='mt-4 rounded-2xl w-full h-68 bg-bg-dark-400 flex items-center justify-center overflow-hidden'>
-              <img className='w-full h-full object-cover' src={image} alt="" />
+            {/* Button */}
+            <div className='flex flex-row gap-2'>
+              <button className='bg-bg-dark-400  pl-4 pr-4 pt-2 pb-2 mt-1 rounded-md text-xs cursor-pointer hover:bg-white/10 flex gap-2'>
+                <PlusIcon className='size-4'></PlusIcon>
+                Add Image
+              </button>
+              <input 
+                type="file" 
+                className='bg-bg-dark-400  pl-4 pr-4 pt-2 pb-2 mt-1 rounded-md text-xs cursor-pointer hover:bg-white/10 flex gap-2'
+
+                >
+                  <PlusIcon className='size-4'></PlusIcon>
+                  Add Image
+                </input>
+
+
+
+              <button className='bg-bg-dark-400  pl-4 pr-4 pt-2 pb-2 mt-1 rounded-md text-xs cursor-pointer hover:bg-white/10'>
+                Submit
+              </button>
+              <button className='bg-bg-dark-400  pl-4 pr-4 pt-2 pb-2 mt-1 rounded-md text-xs cursor-pointer hover:bg-white/10'>
+                Cancel
+              </button>
+
             </div>
-            )
-          }
-          
-
-          {/* Time */}
-          <div className='mt-1'>
-            <p className='text-xs text-white/50'>{dateTime}</p>
-          </div>
-
-           <div className='w-full h-[0.5px] mt-2 bg-white/10'></div>
-
-          {/* Reply button */}
-          <div className='flex flex-row gap-2'>
-            <button className='bg-bg-dark-400  pl-4 pr-4 pt-1 pb-1 mt-1 rounded-md text-xs cursor-pointer hover:bg-white/10'>
-              Reply
-            </button>
-            <button className='bg-bg-dark-400  pl-4 pr-4 pt-1 pb-1 mt-1 rounded-md text-xs cursor-pointer hover:bg-white/10'>
-              Submit
-            </button>
-            <button className='bg-bg-dark-400  pl-4 pr-4 pt-1 pb-1 mt-1 rounded-md text-xs cursor-pointer hover:bg-white/10'>
-              Cancel
-            </button>
-          </div>
-
 
         </div>
 
