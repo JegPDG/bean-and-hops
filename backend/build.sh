ce0adab7bf18ab757ebae6defca7e107c5c6698e
Jeg Paduga <145212230+JegPDG@users.noreply.github.com>
2025-10-04 17:10:22 +0800

    update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -7,3 +7,10 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+User.objects.filter(is_superuser=True).exists() or
+User.objects.create_superuser('admin', 'admin@example.com', 'admin1234')
+"
